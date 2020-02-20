c5d9c6cda167aed534b81c14483262ce60ae9376
Juraj Hilje <juraj@ivpn.net>
2020-02-20 10:46:31 +0100

    feature(control panel): create ControlPanelViewController.swift

---
+++ IVPNClient/Scenes/MainScreen/ControlPanel/ControlPanelViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ControlPanelViewController.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 20/02/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import UIKit
+
+class ControlPanelViewController: UITableViewController {
+    
+    // MARK: - View lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
