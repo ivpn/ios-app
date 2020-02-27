6244d67bcfc386255f2629003b9baaa274c55ed7
Juraj Hilje <juraj@ivpn.net>
2020-02-27 12:12:59 +0100

    feat(control panel): dark mode for ControlPanel view, create ConnectionInfoBoxView.swift

---
+++ IVPNClient/Scenes/MainScreen/ControlPanel/Views/ConnectionInfoBoxView.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionInfoBoxView.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 27/02/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionInfoBoxView: UIView {
+    
+    override func layoutSubviews() {
+        layer.shadowColor = UIColor.init(named: Theme.Key.ivpnGray15)!.cgColor
+    }
+    
+}
