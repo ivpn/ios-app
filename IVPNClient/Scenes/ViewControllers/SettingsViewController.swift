4a1601d970910664aedb55d34e70640721d23fe9
Juraj Hilje <juraj@ivpn.net>
2020-03-04 11:27:22 +0100

    feat(control panel): update Close action selector in SettingsViewController

--- IVPNClient/Scenes/ViewControllers/SettingsViewController.swift
+++ IVPNClient/Scenes/ViewControllers/SettingsViewController.swift
@@ -42,9 +42,9 @@ class SettingsViewController: UITableViewController {
     // MARK: - @IBActions -
     
     @IBAction func close(_ sender: Any) {
-        navigationController?.dismiss(animated: true)
         NotificationCenter.default.post(name: Notification.Name.UpdateFloatingPanelLayout, object: nil)
         NotificationCenter.default.post(name: Notification.Name.UpdateControlPanel, object: nil)
+        navigationController?.dismiss(animated: true)
     }
     
     @IBAction func toggleMultiHop(_ sender: UISwitch) {
