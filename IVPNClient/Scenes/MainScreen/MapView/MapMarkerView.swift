52b051eea29b3bfd14e50977ec2d6ba8573d41fc
Juraj Hilje <juraj@ivpn.net>
2020-03-17 12:57:03 +0100

    feat(map view): add circle setup methods in MapMarkerView

--- IVPNClient/Scenes/MainScreen/MapView/MapMarkerView.swift
+++ IVPNClient/Scenes/MainScreen/MapView/MapMarkerView.swift
@@ -31,30 +31,8 @@ class MapMarkerView: UIView {
     
     override func updateConstraints() {
         setupConstraints()
-        
-        addSubview(circle1)
-        circle1.bb.center().size(width: 187, height: 187)
-        circle1.layer.cornerRadius = 187 / 2
-        circle1.clipsToBounds = true
-        circle1.backgroundColor = blueColor.withAlphaComponent(0.1)
-        
-        addSubview(circle2)
-        circle2.bb.center().size(width: 97, height: 97)
-        circle2.layer.cornerRadius = 97 / 2
-        circle2.clipsToBounds = true
-        circle2.backgroundColor = blueColor.withAlphaComponent(0.3)
-        
-        addSubview(circle3)
-        circle3.bb.center().size(width: 41, height: 41)
-        circle3.layer.cornerRadius = 41 / 2
-        circle3.clipsToBounds = true
-        circle3.backgroundColor = blueColor.withAlphaComponent(0.5)
-        
-        addSubview(circle4)
-        circle4.bb.center().size(width: 9, height: 9)
-        circle4.layer.cornerRadius = 9 / 2
-        circle4.clipsToBounds = true
-        circle4.backgroundColor = blueColor
+        initCircles()
+        updateCircles(color: blueColor)
         
         super.updateConstraints()
     }
@@ -63,6 +41,31 @@ class MapMarkerView: UIView {
     
     private func setupConstraints() {
         bb.center().size(width: 187, height: 187)
+    }
+    
+    private func initCircle(_ circle: UIView, radius: CGFloat) {
+        addSubview(circle)
+        circle.bb.center().size(width: radius, height: radius)
+        circle.layer.cornerRadius = radius / 2
+        circle.clipsToBounds = true
+    }
+    
+    private func updateCircle(_ circle: UIView, color: UIColor) {
+        circle.backgroundColor = color
+    }
+    
+    private func initCircles() {
+        initCircle(circle1, radius: 187)
+        initCircle(circle2, radius: 97)
+        initCircle(circle3, radius: 41)
+        initCircle(circle4, radius: 9)
+    }
+    
+    private func updateCircles(color: UIColor) {
+        updateCircle(circle1, color: color.withAlphaComponent(0.1))
+        updateCircle(circle2, color: color.withAlphaComponent(0.3))
+        updateCircle(circle3, color: color.withAlphaComponent(0.5))
+        updateCircle(circle4, color: color)
     }
     
 }
