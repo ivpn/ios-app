66e02e086363b7c4b27050156eb884a676ea85f4
Juraj Hilje <juraj@ivpn.net>
2020-03-18 12:40:40 +0100

    feat(map view): create ConnectionInfoPopupView.swift

--- IVPNClient/Scenes/MainScreen/MapView/ConnectionInfoPopupView.swift
+++ IVPNClient/Scenes/MainScreen/MapView/ConnectionInfoPopupView.swift
@@ -7,9 +7,65 @@
 //
 
 import UIKit
+import Bamboo
 
 class ConnectionInfoPopupView: UIView {
     
+    // MARK: - View lifecycle -
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    override func updateConstraints() {
+        setupConstraints()
+        super.updateConstraints()
+    }
+    
+    // MARK: - Private methods -
+    
+    private func setupConstraints() {
+        bb.size(width: 270, height: 69).centerX().bottom(15)
+    }
+    
+    private func setupView() {
+        backgroundColor = UIColor.init(named: Theme.Key.ivpnBackgroundPrimary)
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        
+        addSubsviews()
+    }
+    
+    private func addSubsviews() {
+        let statusLabel = UILabel()
+        statusLabel.font = UIFont.systemFont(ofSize: 12)
+        statusLabel.textColor = UIColor.init(named: Theme.Key.ivpnLabel5)
+        statusLabel.text = "Everyone knows about your location"
+        
+        let locationLabel = UILabel()
+        locationLabel.font = UIFont.systemFont(ofSize: 16)
+        locationLabel.textColor = UIColor.init(named: Theme.Key.ivpnLabelPrimary)
+        locationLabel.text = "Kyiv, UA"
+        
+        let actionButton = UIButton()
+        actionButton.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        actionButton.backgroundColor = UIColor.init(named: Theme.Key.ivpnBlue)
+        actionButton.layer.cornerRadius = 10
+        actionButton.clipsToBounds = true
+        
+        addSubview(statusLabel)
+        addSubview(locationLabel)
+        addSubview(actionButton)
+        
+        statusLabel.bb.left(18).top(15).right(-18).height(14)
+        locationLabel.bb.left(18).bottom(-15).right(-48).height(19)
+        actionButton.bb.size(width: 20, height: 20).bottom(-15).right(-18)
+    }
     
 }
--- IVPNClient/Scenes/MainScreen/MapView/MapMarkerView.swift
+++ IVPNClient/Scenes/MainScreen/MapView/MapMarkerView.swift
@@ -31,6 +31,7 @@ class MapMarkerView: UIView {
     private var circle2 = UIView()
     private var circle3 = UIView()
     private var circle4 = UIView()
+    private var connectionInfoPopup = ConnectionInfoPopupView()
     private var blueColor = UIColor.init(red: 68, green: 156, blue: 248)
     private var redColor = UIColor.init(red: 255, green: 98, blue: 88)
     private var grayColor = UIColor.init(red: 211, green: 211, blue: 211)
@@ -42,6 +43,7 @@ class MapMarkerView: UIView {
         initCircles()
         updateCircles(color: blueColor)
         initActionButton()
+        initConnectionInfoPopup()
         
         super.updateConstraints()
     }
@@ -86,6 +88,10 @@ class MapMarkerView: UIView {
         addSubview(actionButton)
         actionButton.bb.fill()
         actionButton.addTarget(self, action: #selector(markerAction), for: .touchUpInside)
+    }
+    
+    private func initConnectionInfoPopup() {
+        addSubview(connectionInfoPopup)
     }
     
     @objc private func markerAction() {
