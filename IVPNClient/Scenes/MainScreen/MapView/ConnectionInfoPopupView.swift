f91d7713cbd0ec74bc27d089d5bfbbc3a2975c4f
Juraj Hilje <juraj@ivpn.net>
2020-03-18 13:35:15 +0100

    feat(map view): update view components in ConnectionInfoPopupView

--- IVPNClient/Scenes/MainScreen/MapView/ConnectionInfoPopupView.swift
+++ IVPNClient/Scenes/MainScreen/MapView/ConnectionInfoPopupView.swift
@@ -11,6 +11,35 @@ import Bamboo
 
 class ConnectionInfoPopupView: UIView {
     
+    // MARK: - View components -
+    
+    lazy var container: UIView = {
+        let container = UIView(frame: .zero)
+        return container
+    }()
+    
+    lazy var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+        statusLabel.font = UIFont.systemFont(ofSize: 12)
+        statusLabel.text = "Everyone knows about your location"
+        statusLabel.textColor = UIColor.init(named: Theme.Key.ivpnLabel5)
+        return statusLabel
+    }()
+    
+    lazy var locationLabel: UILabel = {
+        let locationLabel = UILabel()
+        locationLabel.font = UIFont.systemFont(ofSize: 16)
+        locationLabel.iconMirror(text: "Australia", image: UIImage(named: "au"), alignment: .left)
+        locationLabel.textColor = UIColor.init(named: Theme.Key.ivpnLabelPrimary)
+        return locationLabel
+    }()
+    
+    lazy var actionButton: UIButton = {
+        let actionButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        actionButton.backgroundColor = UIColor.init(named: Theme.Key.ivpnBlue)
+        return actionButton
+    }()
+    
     // MARK: - View lifecycle -
     
     override init(frame: CGRect) {
@@ -43,26 +72,16 @@ class ConnectionInfoPopupView: UIView {
     }
     
     private func addSubsviews() {
-        let statusLabel = UILabel()
-        statusLabel.font = UIFont.systemFont(ofSize: 12)
-        statusLabel.textColor = UIColor.init(named: Theme.Key.ivpnLabel5)
-        statusLabel.text = "Everyone knows about your location"
+        container.addSubview(statusLabel)
+        container.addSubview(locationLabel)
+        container.addSubview(actionButton)
+        addSubview(container)
         
-        let locationLabel = UILabel()
-        locationLabel.font = UIFont.systemFont(ofSize: 16)
-        locationLabel.textColor = UIColor.init(named: Theme.Key.ivpnLabelPrimary)
-        locationLabel.text = "Kyiv, UA"
-        
-        let actionButton = UIButton()
-        actionButton.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        actionButton.backgroundColor = UIColor.init(named: Theme.Key.ivpnBlue)
-        actionButton.layer.cornerRadius = 10
-        actionButton.clipsToBounds = true
-        
-        addSubview(statusLabel)
-        addSubview(locationLabel)
-        addSubview(actionButton)
-        
+        setupSubsviewsConstraints()
+    }
+    
+    private func setupSubsviewsConstraints() {
+        container.bb.fill()
         statusLabel.bb.left(18).top(15).right(-18).height(14)
         locationLabel.bb.left(18).bottom(-15).right(-48).height(19)
         actionButton.bb.size(width: 20, height: 20).bottom(-15).right(-18)
