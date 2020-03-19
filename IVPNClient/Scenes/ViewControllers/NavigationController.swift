1434fadbde67b35d701883458d2a3afc2d0a7bef
Juraj Hilje <juraj@ivpn.net>
2020-03-19 15:40:57 +0100

    feat(signup): update appearance in NavigationController

--- IVPNClient/Scenes/ViewControllers/NavigationController.swift
+++ IVPNClient/Scenes/ViewControllers/NavigationController.swift
@@ -13,9 +13,9 @@ class NavigationController: UINavigationController {
     override func viewDidLoad () {
         super.viewDidLoad()
         
-        UINavigationBar.appearance().tintColor = UIColor.init(named: Theme.Key.ivpnNavigationTint)
-        navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.init(named: Theme.Key.ivpnLabelPrimary)!]
+        UINavigationBar.appearance().tintColor = UIColor.init(named: Theme.Key.ivpnBlue)
         navigationBar.barTintColor = UIColor.init(named: Theme.Key.ivpnBackgroundPrimary)
+        navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .heavy), NSAttributedString.Key.foregroundColor: UIColor.init(named: Theme.Key.ivpnLabelPrimary)!]
     }
     
 }
