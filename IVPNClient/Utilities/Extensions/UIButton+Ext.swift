802253738da565a92a8caeb20bcf3bced1588a80
Juraj Hilje <juraj@ivpn.net>
2020-03-23 12:35:56 +0100

    feat(main screen): create UIButton+Ext.swift

---
+++ IVPNClient/Utilities/Extensions/UIButton+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton+Ext.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 23/03/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setupIcon(imageName: String) {
+        setImage(UIImage.init(named: imageName), for: .normal)
+        backgroundColor = UIColor.init(named: Theme.Key.ivpnBackgroundPrimary)
+        layer.cornerRadius = frame.size.width / 2
+        clipsToBounds = true
+    }
+    
+}
