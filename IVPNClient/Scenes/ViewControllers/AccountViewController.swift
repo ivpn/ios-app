dfe456be6d3cbc8af2a91d16f7ab10640dee17f1
Juraj Hilje <juraj@ivpn.net>
2020-03-23 15:16:38 +0100

    feat(main screen): create AccountViewController.swift

---
+++ IVPNClient/Scenes/ViewControllers/AccountViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AccountViewController.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 23/03/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    // MARK: - View Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initNavigationBar()
+    }
+    
+    // MARK: - Methods -
+    
+    private func initNavigationBar() {
+        if isPresentedModally {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(dismissViewController(_:)))
+        }
+    }
+    
+}
