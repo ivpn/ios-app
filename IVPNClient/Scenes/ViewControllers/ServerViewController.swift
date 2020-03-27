c888a17e08175015f2d48f5946e3f7a3e51937f3
Juraj Hilje <juraj@ivpn.net>
2020-03-27 10:01:32 +0100

    feat(settings): update UITableViewDataSource protocol in ServerViewController

--- IVPNClient/Scenes/ViewControllers/ServerViewController.swift
+++ IVPNClient/Scenes/ViewControllers/ServerViewController.swift
@@ -113,10 +113,6 @@ class ServerViewController: UITableViewController {
 
 extension ServerViewController {
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return collection.count
     }
@@ -132,10 +128,6 @@ extension ServerViewController {
         cell.selectedServer = isExitServer ? Application.shared.settings.selectedExitServer : Application.shared.settings.selectedServer
         
         return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Select Server"
     }
     
 }
