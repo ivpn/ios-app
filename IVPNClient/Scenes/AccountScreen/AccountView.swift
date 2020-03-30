a95a059b9c7eb51e5e4b2a14a003ac01f9218b9e
Juraj Hilje <juraj@ivpn.net>
2020-03-30 10:26:06 +0200

    feat(account): create AccountView.swift

---
+++ IVPNClient/Scenes/AccountScreen/AccountView.swift
@@ -0,0 +1,44 @@
+//
+//  AccountView.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 30/03/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import UIKit
+
+class AccountView: UITableView {
+    
+    // MARK: - @IBOutlets -
+    
+    @IBOutlet weak var qrCodeImage: UIImageView!
+    @IBOutlet weak var accountIdLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var subscriptionLabel: UILabel!
+    @IBOutlet weak var billingCycleLabel: UILabel!
+    @IBOutlet weak var nextBillingLabel: UILabel!
+    @IBOutlet weak var recurringAmountLabel: UILabel!
+    @IBOutlet weak var logOutActionButton: UIButton!
+    @IBOutlet weak var subscriptionActionButton: UIButton!
+    
+    // MARK: - Methods -
+    
+    func setupView(viewModel: AccountViewModel) {
+        accountIdLabel.text = viewModel.accountId
+        statusLabel.text = viewModel.statusText
+        statusLabel.backgroundColor = viewModel.statusColor
+        subscriptionLabel.text = viewModel.subscriptionText
+        billingCycleLabel.text = viewModel.billingCycleText
+        nextBillingLabel.text = viewModel.nextBillingText
+        recurringAmountLabel.text = viewModel.recurringAmountText
+        logOutActionButton.setTitle(viewModel.logOutActionText, for: .normal)
+        subscriptionActionButton.setTitle(viewModel.subscriptionActionText, for: .normal)
+        subscriptionActionButton.isHidden = !viewModel.showSubscriptionAction
+    }
+    
+    func initQRCode(viewModel: AccountViewModel) {
+        qrCodeImage.image = UIImage.generateQRCode(from: viewModel.accountId)
+    }
+    
+}
