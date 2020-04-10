d260c162de40277b329d973951b4a322e0dfeecf
Juraj Hilje <juraj@ivpn.net>
2020-04-10 13:52:48 +0200

    feat(map): evaluate previous VPN status in ConnectionInfoPopupView

--- IVPNClient/Scenes/MainScreen/Map/ConnectionInfoPopupView.swift
+++ IVPNClient/Scenes/MainScreen/Map/ConnectionInfoPopupView.swift
@@ -86,7 +86,7 @@ class ConnectionInfoPopupView: UIView {
     
     var vpnStatusViewModel = VPNStatusViewModel(status: .invalid) {
         didSet {
-            if displayMode == .content && (vpnStatusViewModel.status == .connected || vpnStatusViewModel.status == .disconnected) && !Application.shared.connectionManager.reconnectAutomatically {
+            if displayMode == .content && (vpnStatusViewModel.status == .connected || (vpnStatusViewModel.status == .disconnected && oldValue.status != .disconnected)) && !Application.shared.connectionManager.reconnectAutomatically {
                 DispatchQueue.delay(0.25) {
                     self.show()
                 }
