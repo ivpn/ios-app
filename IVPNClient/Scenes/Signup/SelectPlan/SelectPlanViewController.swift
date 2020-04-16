cb087372ba4283a97bf7314dfa4aa379e2d6d6b6
Juraj Hilje <juraj@ivpn.net>
2020-04-16 09:32:23 +0200

    feat(signup): update navogation bar appearance

--- IVPNClient/Scenes/Signup/SelectPlan/SelectPlanViewController.swift
+++ IVPNClient/Scenes/Signup/SelectPlan/SelectPlanViewController.swift
@@ -14,7 +14,23 @@ class SelectPlanViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initNavigation()
+    }
+    
+    // MARK: - Private methods -
+    
+    private func initNavigation() {
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "icon-arrow-left"), for: .normal)
+        button.sizeToFit()
+        button.addTarget(self, action: #selector(backButtonPressed(sender:)), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+    }
+    
+    @objc private func backButtonPressed(sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
     }
     
 }
