6122c304b10a1ed05cf2d58bff96a876aa69ead4
Juraj Hilje <juraj@ivpn.net>
2020-04-22 09:23:03 +0200

    feat(signup): add purchase method in PaymentViewController

--- IVPNClient/Scenes/Signup/Payment/PaymentViewController.swift
+++ IVPNClient/Scenes/Signup/Payment/PaymentViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyStoreKit
 
 class PaymentViewController: UITableViewController {
     
@@ -17,11 +18,25 @@ class PaymentViewController: UITableViewController {
     // MARK: - Properties -
     
     var collection: [SubscriptionType] = []
+    var selectedSubscription: SubscriptionType = .standard(.monthly)
+    
+    var deviceCanMakePurchases: Bool {
+        guard IAPManager.shared.canMakePurchases else {
+            showAlert(title: "Error", message: "In-App Purchases are not available on your device.")
+            return false
+        }
+        
+        return true
+    }
     
     // MARK: - @IBActions -
     
     @IBAction func goBack() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func purchase(_ sender: UIButton) {
+        purchaseProduct(identifier: selectedSubscription.getProductId())
     }
     
     // MARK: - View lifecycle -
@@ -44,6 +59,47 @@ class PaymentViewController: UITableViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
     }
     
+    private func purchaseProduct(identifier: String) {
+        guard deviceCanMakePurchases else { return }
+        
+        ProgressIndicator.shared.showIn(view: view)
+        
+        IAPManager.shared.purchaseProduct(identifier: identifier) { [weak self] purchase, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.showErrorAlert(title: "Error", message: error)
+                ProgressIndicator.shared.hide()
+                return
+            }
+            
+            if let purchase = purchase {
+                self.completePurchase(purchase: purchase)
+            }
+        }
+    }
+    
+    private func completePurchase(purchase: PurchaseDetails) {
+        IAPManager.shared.completePurchase(purchase: purchase) { [weak self] serviceStatus, error in
+            guard let self = self else { return }
+            
+            ProgressIndicator.shared.hide()
+            
+            if let error = error {
+                self.showErrorAlert(title: "Error", message: error.message) { _ in
+                    if error.status == 400 {
+                        self.navigationController?.dismiss(animated: true, completion: nil)
+                    }
+                }
+                return
+            }
+            
+            if let serviceStatus = serviceStatus {
+                self.showSubscriptionActivatedAlert(serviceStatus: serviceStatus)
+            }
+        }
+    }
+    
 }
 
 // MARK: - UITableViewDelegate -
@@ -56,8 +112,10 @@ extension PaymentViewController {
             paymentView.period = .week
         case 2:
             paymentView.period = .month
+            selectedSubscription = collection[0]
         case 3:
             paymentView.period = .year
+            selectedSubscription = collection[1]
         default:
             break
         }
