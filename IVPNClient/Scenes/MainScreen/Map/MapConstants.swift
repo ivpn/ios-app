a25a00665c3db1979632c68a3a6a20cc918e4cab
Juraj Hilje <juraj@ivpn.net>
2020-04-24 11:12:48 +0200

    feat(map): create MapConstants.swift

---
+++ IVPNClient/Scenes/MainScreen/Map/MapConstants.swift
@@ -0,0 +1,56 @@
+//
+//  MapConstants.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 24/04/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import UIKit
+
+public class MapConstants {
+    
+    public class Container {
+        static let leftAnchor = 0
+        static let topAnchor = 25
+        static let bottomAnchorA = 230
+        static let bottomAnchorB = 274
+        static let bottomAnchorC = 359
+        static let iPadLandscapeLeftAnchor = 375
+        static let iPadLandscapeTopAnchor = 0
+        static let iPadLandscapeBottomAnchor = 0
+        
+        static func getTopAnchor() -> Int {
+            if UIDevice.current.userInterfaceIdiom == .pad && UIApplication.shared.statusBarOrientation.isLandscape {
+                return iPadLandscapeTopAnchor
+            }
+            
+            return topAnchor
+        }
+        
+        static func getLeftAnchor() -> Int {
+            if UIDevice.current.userInterfaceIdiom == .pad && UIApplication.shared.statusBarOrientation.isLandscape {
+                return iPadLandscapeLeftAnchor
+            }
+            
+            return leftAnchor
+        }
+        
+        static func getBottomAnchor() -> Int {
+            if UIDevice.current.userInterfaceIdiom == .pad && UIApplication.shared.statusBarOrientation.isLandscape {
+                return iPadLandscapeBottomAnchor
+            }
+            
+            if Application.shared.settings.connectionProtocol.tunnelType() == .openvpn && UserDefaults.shared.isMultiHop {
+                return bottomAnchorC
+            }
+            
+            if Application.shared.settings.connectionProtocol.tunnelType() == .openvpn {
+                return bottomAnchorB
+            }
+            
+            return bottomAnchorA
+        }
+    }
+    
+}
