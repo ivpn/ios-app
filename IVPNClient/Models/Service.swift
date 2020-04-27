29675f0a9f647333f939cb4877f286489cb264bc
Juraj Hilje <juraj@ivpn.net>
2020-04-27 14:33:50 +0200

    feat(signup): update Service model

--- IVPNClient/Models/Service.swift
+++ IVPNClient/Models/Service.swift
@@ -22,6 +22,61 @@ enum ServiceDuration {
 }
 
 struct Service {
+    
     var type: ServiceType
     var duration: ServiceDuration
+    var productId: String
+    
+    func priceText() -> String {
+        guard !IAPManager.shared.products.isEmpty else { return "" }
+        guard let product = IAPManager.shared.getProduct(identifier: productId) else { return "" }
+        return IAPManager.shared.productPrice(product: product)
+    }
+    
+    func durationText() -> String {
+        switch duration {
+        case .week:
+            return "week"
+        case .month:
+            return "month"
+        case .year:
+            return "year"
+        case .twoYears:
+            return "2 years"
+        case .threeYears:
+            return "3 years"
+        }
+    }
+    
+    func discountText() -> String? {
+        switch type {
+        case .standard:
+            switch duration {
+            case .week:
+                return nil
+            case .month:
+                return "-25%"
+            case .year:
+                return "-42%"
+            case .twoYears:
+                return "-51%"
+            case .threeYears:
+                return "-55%"
+            }
+        case .pro:
+            switch duration {
+            case .week:
+                return nil
+            case .month:
+                return "-16%"
+            case .year:
+                return "-35%"
+            case .twoYears:
+                return "-48%"
+            case .threeYears:
+                return "-52%"
+            }
+        }
+    }
+    
 }
