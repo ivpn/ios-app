ed3e2a10a759045ac89d0d1159ffb28868b6b73f
Juraj Hilje <juraj@ivpn.net>
2020-05-05 13:26:19 +0200

    feat(signup): move ServiceType and ServiceDuration enums to separate files

---
+++ IVPNClient/Enums/ServiceDuration.swift
@@ -0,0 +1,38 @@
+//
+//  ServiceDuration.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 05/05/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import Foundation
+
+enum ServiceDuration: CaseIterable {
+    
+    case week
+    case month
+    case year
+    case twoYears
+    case threeYears
+    
+    func activeUntilFrom(date: Date) -> Date {
+        var dateComponent = DateComponents()
+        
+        switch self {
+        case .week:
+            dateComponent.day = 7
+        case .month:
+            dateComponent.month = 1
+        case .year:
+            dateComponent.year = 1
+        case .twoYears:
+            dateComponent.year = 2
+        case .threeYears:
+            dateComponent.year = 3
+        }
+        
+        return Calendar.current.date(byAdding: dateComponent, to: date) ?? date
+    }
+    
+}
---
+++ IVPNClient/Enums/ServiceType.swift
@@ -0,0 +1,14 @@
+//
+//  ServiceType.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 05/05/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import Foundation
+
+enum ServiceType {
+    case standard
+    case pro
+}
