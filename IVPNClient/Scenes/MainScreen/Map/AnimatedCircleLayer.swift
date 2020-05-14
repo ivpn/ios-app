db2a36ce5bf4d9bb8154d7b8b1512eb1a3a6260f
Juraj Hilje <juraj@ivpn.net>
2020-05-14 09:32:31 +0200

    feat(map): add annotations in AnimatedCircleLayer

--- IVPNClient/Scenes/MainScreen/Map/AnimatedCircleLayer.swift
+++ IVPNClient/Scenes/MainScreen/Map/AnimatedCircleLayer.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AnimatedCircleLayer: CAShapeLayer {
     
+    // MARK: - Properties -
+    
     private var circle: UIBezierPath {
         return UIBezierPath(
             arcCenter: CGPoint(x: CircleParameters.circleSize / 2, y: CircleParameters.circleSize / 2),
@@ -18,6 +20,8 @@ class AnimatedCircleLayer: CAShapeLayer {
             endAngle: CGFloat(.pi * 2.0),
             clockwise: true)
     }
+    
+    // MARK: - Initialize -
     
     override init() {
         super.init()
@@ -30,6 +34,8 @@ class AnimatedCircleLayer: CAShapeLayer {
         circleLayer.path = circle.cgPath
         circleLayer.fillColor = UIColor.init(red: 68, green: 156, blue: 248).cgColor
         addSublayer(circleLayer)
+        
+        startAnimation()
     }
     
     override init(layer: Any) {
@@ -39,6 +45,8 @@ class AnimatedCircleLayer: CAShapeLayer {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Private methods -
     
     func startAnimation() {
         let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
