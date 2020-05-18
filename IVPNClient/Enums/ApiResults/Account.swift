35f5dde9bb5c12b9ebcdda1636fe08b5a047d3d1
Juraj Hilje <juraj@ivpn.net>
2020-05-18 14:10:46 +0200

    feat(signup): add Account API result struct

---
+++ IVPNClient/Enums/ApiResults/Account.swift
@@ -0,0 +1,13 @@
+//
+//  Account.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 18/05/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import Foundation
+
+struct Account: Decodable {
+    let accountId: String
+}
