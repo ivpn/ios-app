a6a9be9cb99e572d9fc62e4697e26b064019b38a
Juraj Hilje <juraj@ivpn.net>
2020-05-27 13:02:10 +0200

    feat(signup): update App Store product IDs

--- IVPNClient/Models/ProductIdentifier.swift
+++ IVPNClient/Models/ProductIdentifier.swift
@@ -10,16 +10,16 @@ import Foundation
 
 struct ProductIdentifier {
     
-    static let standardWeek = "net.ivpn.subscriptions.standard.week"
-    static let standardMonth = "net.ivpn.subscriptions.standard.month"
-    static let standardYear = "net.ivpn.subscriptions.standard.year"
-    static let standardTwoYears = "net.ivpn.subscriptions.standard.twoYears"
-    static let standardThreeYears = "net.ivpn.subscriptions.standard.threeYears"
-    static let proWeek = "net.ivpn.subscriptions.pro.week"
-    static let proMonth = "net.ivpn.subscriptions.pro.month"
-    static let proYear = "net.ivpn.subscriptions.pro.year"
-    static let proTwoYears = "net.ivpn.subscriptions.pro.twoYears"
-    static let proThreeYears = "net.ivpn.subscriptions.pro.threeYears"
+    static let standardWeek = "net.ivpn.subscriptions.standard.1week"
+    static let standardMonth = "net.ivpn.subscriptions.standard.1month"
+    static let standardYear = "net.ivpn.subscriptions.standard.1year"
+    static let standardTwoYears = "net.ivpn.subscriptions.standard.2year"
+    static let standardThreeYears = "net.ivpn.subscriptions.standard.3year"
+    static let proWeek = "net.ivpn.subscriptions.pro.1week"
+    static let proMonth = "net.ivpn.subscriptions.pro.1month"
+    static let proYear = "net.ivpn.subscriptions.pro.1year"
+    static let proTwoYears = "net.ivpn.subscriptions.pro.2year"
+    static let proThreeYears = "net.ivpn.subscriptions.pro.3year"
     
     static var all: Set<String> {
         return [
