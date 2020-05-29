43272b270a68b99adcabfea6e6a0f46a3c943612
Juraj Hilje <juraj@ivpn.net>
2020-05-29 10:09:43 +0200

    feat(settings): create ServersSort enum

---
+++ IVPNClient/Enums/ServersSort.swift
@@ -0,0 +1,15 @@
+//
+//  ServersSort.swift
+//  IVPNClient
+//
+//  Created by Juraj Hilje on 29/05/2020.
+//  Copyright © 2020 IVPN. All rights reserved.
+//
+
+import Foundation
+
+enum ServersSort: String {
+    case countryCode
+    case city
+    case latency
+}
