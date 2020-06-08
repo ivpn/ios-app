a9b1105526a71688a5aeceb5152347b231210e70
Juraj Hilje <juraj@ivpn.net>
2020-06-08 16:52:10 +0200

    refactor(map): renamed appDidBecomeActive method in MapMarkerView

--- IVPNClient/Scenes/MainScreen/Map/MapMarkerView.swift
+++ IVPNClient/Scenes/MainScreen/Map/MapMarkerView.swift
@@ -123,9 +123,9 @@ class MapMarkerView: UIView {
         NotificationCenter.default.addObserver(self, selector: #selector(hidePopup), name: Notification.Name.HideConnectionInfoPopup, object: nil)
         
         if #available(iOS 13.0, *) {
-            NotificationCenter.default.addObserver(self, selector: #selector(appMovedToForeground), name: UIScene.didActivateNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(appDidBecomeActive), name: UIScene.didActivateNotification, object: nil)
         } else {
-            NotificationCenter.default.addObserver(self, selector: #selector(appMovedToForeground), name: UIApplication.didBecomeActiveNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(appDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
         }
     }
     
@@ -191,7 +191,7 @@ class MapMarkerView: UIView {
         connectionInfoPopup.hide()
     }
     
-    @objc private func appMovedToForeground() {
+    @objc private func appDidBecomeActive() {
         if displayMode == .protected {
             animatedCircleLayer.startAnimation()
         }
