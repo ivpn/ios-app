03def8a653afa15680650dbc0ce31c09f7a33912
Juraj Hilje <juraj@ivpn.net>
2020-07-08 20:59:32 +0200

    refactor: fix Trailing Semicolon Violation warning in UIView+Ext.swift

--- IVPNClient/Utilities/Extensions/UIView+Ext.swift
+++ IVPNClient/Utilities/Extensions/UIView+Ext.swift
@@ -43,7 +43,7 @@ extension UIView {
     
     func rotate(angle: CGFloat) {
         let radians = angle / 180.0 * CGFloat.pi
-        let rotation = self.transform.rotated(by: radians);
+        let rotation = self.transform.rotated(by: radians)
         self.transform = rotation
     }
     
