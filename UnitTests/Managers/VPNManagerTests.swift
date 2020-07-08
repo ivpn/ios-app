cd0c88073aef34ad6c4a8fc711342ca4a7f91cd3
Juraj Hilje <juraj@ivpn.net>
2020-07-08 19:06:41 +0200

    chrore: add new source file header

--- UnitTests/Managers/VPNManagerTests.swift
+++ UnitTests/Managers/VPNManagerTests.swift
@@ -1,9 +1,24 @@
 //
 //  VPNManagerTests.swift
-//  IVPN Client
+//  IVPN iOS app
+//  https://github.com/ivpn/ios-app
 //
-//  Created by Fedir Nepyyvoda on 11/7/16.
-//  Copyright © 2016 IVPN. All rights reserved.
+//  Created by Fedir Nepyyvoda on 2016-11-07.
+//  Copyright (c) 2020 Privatus Limited.
+//
+//  This file is part of the IVPN iOS app.
+//
+//  The IVPN iOS app is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  The IVPN iOS app is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with the IVPN iOS app. If not, see <https://www.gnu.org/licenses/>.
 //
 
 // FIXME: - Reimplement VPNManagerTests to reflect current VPNManager.swift -
