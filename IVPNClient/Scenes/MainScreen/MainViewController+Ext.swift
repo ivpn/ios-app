5dfe375bc97e5cc00942e025c5918a4f3289b854
Juraj Hilje <juraj@ivpn.net>
2020-07-08 20:43:17 +0200

    refactor: fix Vertical Whitespace Violation warning in MainViewController+Ext.swift

--- IVPNClient/Scenes/MainScreen/MainViewController+Ext.swift
+++ IVPNClient/Scenes/MainScreen/MainViewController+Ext.swift
@@ -43,7 +43,6 @@ extension MainViewController: FloatingPanelControllerDelegate {
     
 }
 
-
 // MARK: - UIAdaptivePresentationControllerDelegate -
 
 extension MainViewController: UIAdaptivePresentationControllerDelegate {
