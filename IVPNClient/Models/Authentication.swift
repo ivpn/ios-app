346ad87881a9b6848bc8f364034d5a597def8e19
Juraj Hilje <juraj@ivpn.net>
2020-07-08 19:23:57 +0200

    chrore: update source file header

--- IVPNClient/Models/Authentication.swift
+++ IVPNClient/Models/Authentication.swift
@@ -22,7 +22,7 @@
 //
 //  The IVPN iOS app is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 //  details.
 //
 //  You should have received a copy of the GNU General Public License
