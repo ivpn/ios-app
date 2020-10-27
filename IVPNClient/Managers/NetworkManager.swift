f15e3556e935faf54864fc21484adf8f05b2b779
Juraj Hilje <juraj@ivpn.net>
2020-10-27 10:57:50 +0100

    fix: update default network name in startMonitoring method

--- IVPNClient/Managers/NetworkManager.swift
+++ IVPNClient/Managers/NetworkManager.swift
@@ -53,7 +53,7 @@ class NetworkManager {
                     NotificationCenter.default.post(name: Notification.Name.NetworkSaved, object: nil)
                     self.updateNetwork(name: ssid, type: NetworkType.wifi.rawValue)
                 } else if Application.shared.connectionManager.status == .invalid {
-                    self.updateNetwork(name: "WiFi", type: NetworkType.none.rawValue)
+                    self.updateNetwork(name: "Wi-Fi", type: NetworkType.none.rawValue)
                 } else {
                     self.updateNetwork(name: "No network", type: NetworkType.none.rawValue)
                 }
