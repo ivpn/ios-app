04c37765866a470bc1301995c7f4ecf80da273fd
Juraj Hilje <juraj@ivpn.net>
2021-01-26 17:13:57 +0100

    feat(wireguard): revert changes in ProtocolViewController

--- IVPNClient/Scenes/ViewControllers/ProtocolViewController.swift
+++ IVPNClient/Scenes/ViewControllers/ProtocolViewController.swift
@@ -326,7 +326,7 @@ extension ProtocolViewController {
         if AppKeyManager.isKeyExpired {
             showAlert(title: "Failed to automatically rotate WireGuard keys", message: "Cannot connect using WireGuard protocol: rotating WireGuard keys failed. This is likely because of no access to an IVPN API server. You can retry connection, rotate keys manually from preferences, or select another protocol. Please contact support if this error persists.")
         } else {
-            showAlert(title: "Failed to rotate WireGuard keys", message: "There was a problem generating and uploading WireGuard keys to IVPN server.")
+            showAlert(title: "Failed to regenerate WireGuard keys", message: "There was a problem generating and uploading WireGuard keys to IVPN server.")
         }
     }
     
