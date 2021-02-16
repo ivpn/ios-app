dd46079710961c404999267f2c342745f4af933c
Juraj Hilje <juraj@ivpn.net>
2021-02-16 09:47:30 +0100

    feat(secure dns): create SecureDNS model

---
+++ IVPNClient/Models/SecureDNS.swift
@@ -0,0 +1,68 @@
+//
+//  SecureDNS.swift
+//  IVPN iOS app
+//  https://github.com/ivpn/ios-app
+//
+//  Created by Juraj Hilje on 2021-02-16.
+//  Copyright (c) 2021 Privatus Limited.
+//
+//  This file is part of the IVPN iOS app.
+//
+//  The IVPN iOS app is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  The IVPN iOS app is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with the IVPN iOS app. If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+struct SecureDNS: Codable {
+    
+    var ipAddress: String?
+    var type: String
+    var mobileNetwork: Bool
+    var wifiNetwork: Bool
+    
+    private static let secureDNSKey = "SecureDNS"
+    
+    // MARK: - Initialize -
+    
+    init() {
+        let model = SecureDNS.load()
+        ipAddress = model?.ipAddress
+        type = model?.type ?? "doh"
+        mobileNetwork = model?.mobileNetwork ?? true
+        wifiNetwork = model?.wifiNetwork ?? true
+    }
+    
+    // MARK: - Methods -
+    
+    func save() {
+        if let encoded = try? JSONEncoder().encode(self) {
+            UserDefaults.standard.set(encoded, forKey: SecureDNS.secureDNSKey)
+        }
+    }
+    
+    static func load() -> SecureDNS? {
+        if let savedObj = UserDefaults.standard.object(forKey: SecureDNS.secureDNSKey) as? Data {
+            if let loadedObj = try? JSONDecoder().decode(SecureDNS.self, from: savedObj) {
+                return loadedObj
+            }
+        }
+        
+        return nil
+    }
+    
+}
+
+enum SecureDNSType: String {
+    case doh
+    case dot
+}
