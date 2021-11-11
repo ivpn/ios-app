d9bbff7a937551a32970c6e220ee6d6214d4cba5
Juraj Hilje <juraj@ivpn.net>
2021-11-11 19:49:46 +0100

    feat(exclude local network): update UITableViewDelegate in SettingsViewController

--- IVPNClient/Scenes/ViewControllers/SettingsViewController.swift
+++ IVPNClient/Scenes/ViewControllers/SettingsViewController.swift
@@ -454,7 +454,7 @@ extension SettingsViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 3 && indexPath.row == 8 {
+        if indexPath.section == 3 && indexPath.row == 9 {
             tableView.deselectRow(at: indexPath, animated: true)
             sendLogs()
         }
