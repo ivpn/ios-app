dcaa193ebec029a86173838a8a23e0f3555ba76d
Juraj Hilje <juraj@ivpn.net>
2022-07-04 20:45:58 +0200

    feat: update ServerTableViewCell

--- IVPNClient/Scenes/TableCells/ServerTableViewCell.swift
+++ IVPNClient/Scenes/TableCells/ServerTableViewCell.swift
@@ -32,6 +32,7 @@ class ServerTableViewCell: UITableViewCell {
     @IBOutlet weak var pingTimeMs: UILabel!
     @IBOutlet weak var configureButton: UIButton!
     @IBOutlet weak var ipv6Label: UILabel!
+    @IBOutlet weak var expandButton: UIButton!
     
     var viewModel: VPNServerViewModel! {
         didSet {
@@ -41,18 +42,21 @@ class ServerTableViewCell: UITableViewCell {
                 serverName.text = "Fastest server"
                 configureButton.isHidden = false
                 configureButton.isUserInteractionEnabled = true
+                expandButton.isHidden = true
             } else if viewModel.server.isHost {
                 serverName.text = viewModel.formattedServerName
                 configureButton.isHidden = true
                 configureButton.isUserInteractionEnabled = false
                 flagImage.image = nil
                 flagImage.image?.accessibilityIdentifier = ""
+                expandButton.isHidden = true
             } else if isMultiHop && indexPath.row == 0 || !isMultiHop && indexPath.row == 1 {
                 flagImage.image = UIImage(named: "icon-shuffle")
                 flagImage.image?.accessibilityIdentifier = "icon-shuffle"
                 serverName.text = "Random server"
                 configureButton.isHidden = true
                 configureButton.isUserInteractionEnabled = true
+                expandButton.isHidden = true
             } else {
                 let sort = ServersSort.init(rawValue: UserDefaults.shared.serversSort) ?? .city
                 flagImage.image = viewModel.imageForCountryCode
@@ -60,6 +64,7 @@ class ServerTableViewCell: UITableViewCell {
                 serverName.text = viewModel.formattedServerName(sort: sort)
                 configureButton.isHidden = true
                 configureButton.isUserInteractionEnabled = false
+                expandButton.isHidden = false
             }
             flagImage.updateUpFlagIcon()
             serverName.sizeToFit()
