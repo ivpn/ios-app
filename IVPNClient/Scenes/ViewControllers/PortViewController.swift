8ddc923f852ba6b014997874b2728a7bf1a20fd3
Juraj Hilje <juraj@ivpn.net>
2022-07-11 16:36:02 +0200

    feat(settings): create PortViewController.swift

---
+++ IVPNClient/Scenes/ViewControllers/PortViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PortViewController.swift
+//  IVPN iOS app
+//  https://github.com/ivpn/ios-app
+//
+//  Created by Juraj Hilje on 2022-07-11.
+//  Copyright (c) 2022 Privatus Limited.
+//
+//  This file is part of the IVPN iOS app.
+//
+//  The IVPN iOS app is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  The IVPN iOS app is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with the IVPN iOS app. If not, see <https://www.gnu.org/licenses/>.
+//
+
+import UIKit
+
+class PortViewController: UITableViewController {
+    
+    // MARK: - Properties -
+    
+    var viewModel = PortViewModel()
+    var ports = [ConnectionSettings]()
+    var connectionProtocol = ConnectionSettings.wireguard(.udp, 0) {
+        didSet {
+            
+        }
+    }
+    
+    // MARK: - View Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
