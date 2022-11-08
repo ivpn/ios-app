6cb64792d9648f275742b38c5f533841169224ef
Juraj Hilje <juraj.hilje@gmail.com>
2022-11-08 10:04:47 +0100

    feat(settings): update collection in PortViewController.swift

--- IVPNClient/Scenes/ViewControllers/PortViewController.swift
+++ IVPNClient/Scenes/ViewControllers/PortViewController.swift
@@ -39,8 +39,15 @@ class PortViewController: UITableViewController {
     }
     
     var customPorts: [ConnectionSettings] {
-//        return PortRange.getPorts(from: portRanges, tunnelType: selectedPort.formatTitle())
-        return []
+        var ports = [ConnectionSettings]()
+        if let storedCustomPorts = StorageManager.fetchCustomPorts(vpnProtocol: selectedPort.formatTitle().lowercased()) {
+            for customPort in storedCustomPorts {
+                let string = "\(String(describing: customPort.vpnProtocol))-\(String(describing: customPort.type))-\(customPort.port)"
+                ports.append(ConnectionSettings.getFrom(portString: string))
+            }
+        }
+        
+        return ports
     }
     
     var collection: [ConnectionSettings] {
@@ -67,30 +74,6 @@ class PortViewController: UITableViewController {
         title = "Select Port"
         tableView.backgroundColor = UIColor.init(named: Theme.ivpnBackgroundQuaternary)
     }
-    
-//    @objc func cancel() {
-//        view.endEditing(true)
-//        tableView.reloadData()
-//    }
-//
-//    @objc func save() {
-//        if let text = updatedTextField?.text {
-//            let port = Int(text) ?? 0
-//            if let error = updatedPortRange?.save(port: port) {
-//                showAlert(title: "Error", message: error)
-//            }
-//
-//            if port == 0, let firstPort = collection.first {
-//                selectedPort = firstPort
-//            } else {
-//                selectedPort = collection[selectedPortIndex]
-//            }
-//        }
-//
-//        Application.shared.settings.connectionProtocol = selectedPort
-//        view.endEditing(true)
-//        tableView.reloadData()
-//    }
 
 }
 
@@ -111,14 +94,6 @@ extension PortViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.section == 1 {
-//            let range = portRanges[indexPath.row]
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "PortInputTableViewCell", for: indexPath) as! PortInputTableViewCell
-//            cell.setup(range: range)
-//            cell.portInput.delegate = self
-//            return cell
-//        }
-        
         if indexPath.section == 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "AddPortTableViewCell", for: indexPath)
             return cell
@@ -175,48 +150,6 @@ extension PortViewController {
     }
     
 }
-
-// MARK: - UITextFieldDelegate -
-
-//extension PortViewController: UITextFieldDelegate {
-//
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//        save()
-//        return true
-//    }
-//
-//    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancel))
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(save))
-//
-//        var superview = (textField as AnyObject).superview
-//        while let view = superview, !(view is UITableViewCell) {
-//            superview = view?.superview
-//        }
-//        guard let cell = superview as? UITableViewCell else {
-//            return
-//        }
-//        guard let indexPath = tableView.indexPath(for: cell) else {
-//            return
-//        }
-//
-//        updatedPortRange = portRanges[indexPath.row]
-//        updatedTextField = textField
-//    }
-//
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        navigationItem.hidesBackButton = false
-//        navigationItem.leftBarButtonItem = nil
-//        navigationItem.rightBarButtonItem = nil
-//        DispatchQueue.async {
-//            self.updatedPortRange = nil
-//            self.updatedTextField = nil
-//            self.tableView.reloadData()
-//        }
-//    }
-//
-//}
 
 // MARK: - AddCustomPortViewControllerDelegate -
 
