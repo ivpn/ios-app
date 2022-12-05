c27133b94467a73e7245e425d0bbac7f02100592
Juraj Hilje <juraj.hilje@gmail.com>
2022-12-05 13:59:35 +0100

    fix: validate hoststr.count in Ping.swift

--- IVPNClient/Utilities/Pinger/PingManager/Ping.swift
+++ IVPNClient/Utilities/Pinger/PingManager/Ping.swift
@@ -256,12 +256,17 @@ class Ping : NSObject {
         
         //process the data we read.
         if bytesRead > 0 {
-            var hoststr = [CChar](repeating: 0, count: Int(INET_ADDRSTRLEN))
+            var hoststr = [CChar](repeating: 0, count: Int(INET6_ADDRSTRLEN))
             //            char hoststr[INET6_ADDRSTRLEN];
             var sin : sockaddr_in = UnsafeMutableRawPointer(addrSockaddr).bindMemory(to: sockaddr_in.self, capacity: Int(addrLen)).pointee
             inet_ntop(Int32(sin.sin_family), &(sin.sin_addr), &hoststr, socklen_t(INET6_ADDRSTRLEN))
             //            struct sockaddr_in *sin = (struct sockaddr_in *)&addr;
             //            inet_ntop(sin->sin_family, &(sin->sin_addr), hoststr, INET6_ADDRSTRLEN);
+            
+            guard hoststr.count > 0 else {
+                return
+            }
+            
             let host = String(cString: hoststr)
             
             if(host == hostAddressString) { // only make sense where received packet comes from expected source
