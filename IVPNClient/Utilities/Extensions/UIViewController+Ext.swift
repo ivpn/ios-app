fbf2dd6cefb9357d0f77c4866e0a2f7b504d431e
Juraj Hilje <juraj.hilje@gmail.com>
2023-04-25 14:15:24 +0200

    refactor: update topViewController method in UIApplication+Ext.swift

--- IVPNClient/Utilities/Extensions/UIViewController+Ext.swift
+++ IVPNClient/Utilities/Extensions/UIViewController+Ext.swift
@@ -214,6 +214,26 @@ extension UIViewController {
         return true
     }
     
+    func topMostViewController() -> UIViewController {
+        if self.presentedViewController == nil {
+            return self
+        }
+        
+        if let navigation = self.presentedViewController as? UINavigationController {
+            return navigation.visibleViewController!.topMostViewController()
+        }
+        
+        if let tab = self.presentedViewController as? UITabBarController {
+            if let selectedTab = tab.selectedViewController {
+                return selectedTab.topMostViewController()
+            }
+            
+            return tab.topMostViewController()
+        }
+        
+        return self.presentedViewController!.topMostViewController()
+    }
+    
 }
 
 // MARK: - Presenter -
