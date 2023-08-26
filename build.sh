05c09f85d93b563861c8a028f4a224a63c25bc17
Juraj Hilje <juraj.hilje@gmail.com>
2023-08-26 11:38:11 +0200

    build: update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,17 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+V2RAY_VER=v4.35.0
+
 echo "=> ⬇️  Clone V2Ray sources.."
 git submodule update --init
 
 echo "=> ⬇️  Get gomobile.."
-cd vendor/v2ray-core
+cd vendor/v2ray
+git checkout ${V2RAY_VER}
 PATH=$PATH:~/go/bin
 go get golang.org/x/mobile/cmd/gomobile
 
 echo "=> 🍏 Build iOS library.."
-gomobile bind --target=ios -o ../../Frameworks/V2Ray.xcframework
+gomobile bind -trimpath -ldflags "-s -w" --target=ios -o ../../build/ios/V2Ray.xcframework
 echo "=> ✅ iOS build completed"
